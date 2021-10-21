f94fa245c843f7bf7f48f4fe2227a3795da7bea2
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-21 12:19:58 +0530

    Added: welcome message

---
+++ src/main/java/org/blz/binarytree/MyBinaryTree.java
@@ -0,0 +1,10 @@
+package org.blz.binarytree;
+
+
+public class MyBinaryTree
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to binary tree" );
+    }
+}
